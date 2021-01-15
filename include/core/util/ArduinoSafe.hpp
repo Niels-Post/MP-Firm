036e44c57bbaf8c33275c96e31fa0cb4befe9599
niels <niels.post.97@gmail.com>
2021-01-15 16:01:19 +0100

    - Applied GPL v3 license

--- include/core/util/ArduinoSafe.hpp
+++ include/core/util/ArduinoSafe.hpp
@@ -1,4 +1,23 @@
-/**
+/*
+ * Copyright (c) 2021. Niels Post. AI Lab Vrije Universiteit Brussel.
+ *
+ * This file is part of MP-Firm.
+ *
+ * MP-Firm is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * MP-Firm is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with MP-Firm.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+ /**
  * Arduino.h redefines min and max, which causes problems when using certain parts of the C++ standard library.
  * By undefining true and false from Arduino, code completion features in IDE's seem to work better.
  */
